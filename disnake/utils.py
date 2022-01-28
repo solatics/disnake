a8875efd26d81faa2bc29b7875c5f0be8e49174a
onerandomusername <genericusername414+git@gmail.com>
2022-01-28 00:30:17 -0500

    chore: use 'is' or 'is not' to check True or False

--- disnake/utils.py
+++ disnake/utils.py
@@ -1103,7 +1103,7 @@ def evaluate_annotation(
         is_literal = False
         args = tp.__args__
         if not hasattr(tp, "__origin__"):
-            if PY_310 and tp.__class__ is types.UnionType:  # type: ignore
+            if PY_310 and isinstance(tp, types.UnionType):  # type: ignore
                 converted = Union[args]  # type: ignore
                 return evaluate_annotation(converted, globals, locals, cache)
 
